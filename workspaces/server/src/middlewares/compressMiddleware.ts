10e8185051d90169b64158336b56d38a1b368ce5
pirosiki197 <lingmukangtai825@gmail.com>
2024-09-22 05:01:31 +0900

    TopPage をいろいろ

--- workspaces/server/src/middlewares/compressMiddleware.ts
+++ workspaces/server/src/middlewares/compressMiddleware.ts
@@ -2,32 +2,38 @@ import { encoding } from '@hapi/accept';
 import { ZstdInit } from '@oneidentity/zstd-js/asm/index.cjs.js';
 import { createMiddleware } from 'hono/factory';
 
+const compressibleTypes = ['text/html', 'text/plain', 'text/css', 'application/json', 'application/javascript'];
+
 const zstdInit = ZstdInit();
 
 export const compressMiddleware = createMiddleware(async (c, next) => {
   await next();
+
+  const contentType = c.res.headers.get('Content-Type');
+  if (!contentType || !compressibleTypes.some((type) => contentType.startsWith(type))) {
+    return;
+  }
+
   const { ZstdStream } = await zstdInit;
 
   const accept = encoding(c.req.header('X-Accept-Encoding'), ['zstd']);
+  if (accept === 'zstd') {
+    const originalBody = c.res.body;
 
-  switch (accept) {
-    case 'zstd': {
-      const transform = new TransformStream<Uint8Array, Uint8Array>({
-        transform(chunk, controller) {
-          controller.enqueue(ZstdStream.compress(chunk, 12, false));
+    if (originalBody) {
+      const transformStream = new TransformStream<Uint8Array, Uint8Array>({
+        async transform(chunk, controller) {
+          const compressedChunk = ZstdStream.compress(chunk, 12, false);
+          controller.enqueue(compressedChunk);
         },
       });
 
-      c.res = new Response(c.res.body?.pipeThrough(transform), c.res);
+      const compressedStream = originalBody.pipeThrough(transformStream);
+      c.res = new Response(compressedStream, c.res);
 
       c.res.headers.delete('Content-Length');
-      c.res.headers.append('Cache-Control', 'no-transform');
       c.res.headers.set('X-Content-Encoding', 'zstd');
-      break;
-    }
-    default: {
       c.res.headers.append('Cache-Control', 'no-transform');
-      break;
     }
   }
 });
