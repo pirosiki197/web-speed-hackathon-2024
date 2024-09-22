dde13cf0b22fb90a75336bf17b9eed4d00a447a0
pirosiki197 <lingmukangtai825@gmail.com>
2024-09-23 02:31:18 +0900

    はやくなれ

--- workspaces/app/src/pages/TopPage/index.tsx
+++ workspaces/app/src/pages/TopPage/index.tsx
@@ -27,6 +27,10 @@ const TopPage: React.FC = () => {
   const rankingA11yId = useId();
   const todayA11yId = useId();
 
+  if (!release || !featureList || !rankingList) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Flex align="flex-start" direction="column" gap={Space * 2} justify="center" pb={Space * 2}>
       <Box as="header" maxWidth="100%" width="100%">
