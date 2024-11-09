1d0ea278fcf1f642bf3d69852a9a083eb1ac32e5
pirosiki197 <lingmukangtai825@gmail.com>
2024-11-10 01:14:07 +0900

    remove three

--- workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
+++ workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
@@ -1,8 +1,6 @@
 import { useCallback, useEffect, useRef } from 'react';
 import styled from 'styled-components';
-import { Mesh, OrthographicCamera, PlaneGeometry, Scene, ShaderMaterial, TextureLoader, WebGLRenderer } from 'three';
-
-// import { IMAGE_SRC } from './ImageSrc';
+// import { Mesh, OrthographicCamera, PlaneGeometry, Scene, ShaderMaterial, TextureLoader, WebGLRenderer } from 'three';
 
 const _Wrapper = styled.div`
   aspect-ratio: 16 / 9;
@@ -40,54 +38,76 @@ export const HeroImage: React.FC = () => {
     const imageWidth = image.clientWidth;
     const imageHeight = (imageWidth / 16) * 9;
 
-    const scene = new Scene();
-    const camera = new OrthographicCamera(-1, 1, 1, -1, 1, 1000);
-    camera.position.set(0, 0, 100);
-    camera.lookAt(scene.position);
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    if (ctx == null) {
+      return;
+    }
 
-    const textureLoader = new TextureLoader();
+    const img = new Image();
+    img.src = '/assets/hero.webp'
+    img.onload = () => {
+      canvas.width = width;
+      canvas.height = height;
 
-    textureLoader.load('/assets/hero.webp', (texture) => {
-      const geometry = new PlaneGeometry(2, 2);
-      const material = new ShaderMaterial({
-        fragmentShader: `uniform sampler2D tImage;
-varying vec2 vUv;
-void main() {
-  float aspectRatio = float(textureSize(tImage, 0).x / textureSize(tImage, 0).y);
-  vec2 uv = vec2(
-      (vUv.x - 0.5) / min(aspectRatio, 1.0) + 0.5,
-      (vUv.y - 0.5) / min(1.0 / aspectRatio, 1.0) + 0.5
-  );
-  gl_FragColor = texture2D(tImage, vUv);
-}`,
-        uniforms: {
-          tImage: { value: texture },
-        },
-        vertexShader: `varying vec2 vUv;
-void main() {
-  vUv = uv;
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-}`,
-      });
-      const mesh = new Mesh(geometry, material);
-      scene.add(mesh);
-
-      const renderer = new WebGLRenderer({ alpha: true, antialias: true, canvas: canvasRef.current });
-      renderer.setPixelRatio(window.devicePixelRatio);
-      renderer.setSize(width, height);
-
-      const animate = () => {
-        requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-      };
-      animate();
+      ctx.drawImage(img, 0, 0, width, height);
 
       updateImage({
         height: imageHeight,
-        src: canvasRef.current.toDataURL(),
+        src: canvas.toDataURL(),
         width: imageWidth,
       });
-    });
+    };
+
+//     const scene = new Scene();
+//     const camera = new OrthographicCamera(-1, 1, 1, -1, 1, 1000);
+//     camera.position.set(0, 0, 100);
+//     camera.lookAt(scene.position);
+
+//     const textureLoader = new TextureLoader();
+
+//     textureLoader.load('/assets/hero.webp', (texture) => {
+//       const geometry = new PlaneGeometry(2, 2);
+//       const material = new ShaderMaterial({
+//         fragmentShader: `uniform sampler2D tImage;
+// varying vec2 vUv;
+// void main() {
+//   float aspectRatio = float(textureSize(tImage, 0).x / textureSize(tImage, 0).y);
+//   vec2 uv = vec2(
+//       (vUv.x - 0.5) / min(aspectRatio, 1.0) + 0.5,
+//       (vUv.y - 0.5) / min(1.0 / aspectRatio, 1.0) + 0.5
+//   );
+//   gl_FragColor = texture2D(tImage, vUv);
+// }`,
+//         uniforms: {
+//           tImage: { value: texture },
+//         },
+//         vertexShader: `varying vec2 vUv;
+// void main() {
+//   vUv = uv;
+//   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+// }`,
+//       });
+//       const mesh = new Mesh(geometry, material);
+//       scene.add(mesh);
+
+//       const renderer = new WebGLRenderer({ alpha: true, antialias: true, canvas: canvasRef.current });
+//       renderer.setPixelRatio(window.devicePixelRatio);
+//       renderer.setSize(width, height);
+
+//       const animate = () => {
+//         requestAnimationFrame(animate);
+//         renderer.render(scene, camera);
+//       };
+//       animate();
+
+//       updateImage({
+//         height: imageHeight,
+//         src: canvasRef.current.toDataURL(),
+//         width: imageWidth,
+//       });
+//     }
+//  );
   }, [imageRef, updateImage]);
 
   useEffect(() => {
