20ed2dca448a72c6a87c255f0089c8a53be26945
pirosiki197 <lingmukangtai825@gmail.com>
2024-11-10 03:41:59 +0900

    アイコン必要なやつだけ

--- workspaces/app/src/features/icons/components/SvgIcon.tsx
+++ workspaces/app/src/features/icons/components/SvgIcon.tsx
@@ -1,14 +1,33 @@
-import * as Icons from '@mui/icons-material';
+import {ArrowBack ,Close, Favorite, FavoriteBorder, NavigateNext, Search} from '@mui/icons-material';
 
 type Props = {
   color: string;
   height: number;
-  type: keyof typeof Icons;
+  type: 'ArrowBack' | 'Close' | 'NavigateNext' | 'Favorite' | 'FavoriteBorder' | 'Search';
   width: number;
 };
 
 export const SvgIcon: React.FC<Props> = ({ color, height, type, width }) => {
-  // eslint-disable-next-line
-  const Icon = Icons[type];
+  let Icon;
+  switch (type) {
+    case 'ArrowBack':
+      Icon = ArrowBack;
+      break;
+    case 'Close':
+      Icon = Close;
+      break;
+    case 'NavigateNext':
+      Icon = NavigateNext;
+      break;
+    case 'Search':
+      Icon = Search;
+      break;
+    case 'Favorite':
+      Icon = Favorite;
+      break;
+    case 'FavoriteBorder':
+      Icon = FavoriteBorder;
+      break;
+  }
   return <Icon style={{ color, height, width }} />;
 };
