e3a279aa1b8a9543422f09404568d509b7141317
pirosiki197 <lingmukangtai825@gmail.com>
2024-11-10 16:58:40 +0900

    N+1

--- workspaces/app/src/features/episode/components/EpisodeListItem.tsx
+++ workspaces/app/src/features/episode/components/EpisodeListItem.tsx
@@ -7,9 +7,8 @@ import { Link } from '../../../foundation/components/Link';
 import { Separator } from '../../../foundation/components/Separator';
 import { Spacer } from '../../../foundation/components/Spacer';
 import { Text } from '../../../foundation/components/Text';
-import { useImage } from '../../../foundation/hooks/useImage';
 import { Color, Radius, Space, Typography } from '../../../foundation/styles/variables';
-import { useEpisode } from '../hooks/useEpisode';
+import { getImageUrl } from '../../../lib/image/getImageUrl';
 
 const _Wrapper = styled.li`
   width: 100%;
@@ -29,13 +28,20 @@ const _ImgWrapper = styled.div`
 
 type Props = {
   bookId: string;
-  episodeId: string;
+  episode: {
+    chapter: number;
+    description: string;
+    id: string;
+    image: {
+      id: string;
+    };
+    name: string;
+  }
 };
 
-export const EpisodeListItem: React.FC<Props> = ({ bookId, episodeId }) => {
-  const { data: episode } = useEpisode({ params: { episodeId } });
-
-  const imageUrl = useImage({ height: 96, imageId: episode.image.id, width: 96 });
+export const EpisodeListItem: React.FC<Props> = ({ bookId, episode }) => {
+  // const imageUrl = useImage({ height: 96, imageId: episode.image.id, width: 96 });
+  const imageUrl = getImageUrl({format: 'webp', imageId: episode.image.id});
 
   return (
     <_Wrapper>
--- workspaces/app/src/pages/EpisodeDetailPage/index.tsx
+++ workspaces/app/src/pages/EpisodeDetailPage/index.tsx
@@ -3,9 +3,9 @@ import { useParams } from 'react-router-dom';
 import type { RouteParams } from 'regexparam';
 import invariant from 'tiny-invariant';
 
-import { useBook } from '../../features/book/hooks/useBook';
 import { EpisodeListItem } from '../../features/episode/components/EpisodeListItem';
 import { useEpisode } from '../../features/episode/hooks/useEpisode';
+import { useEpisodeList } from '../../features/episode/hooks/useEpisodeList';
 import { Box } from '../../foundation/components/Box';
 import { Flex } from '../../foundation/components/Flex';
 import { Separator } from '../../foundation/components/Separator';
@@ -13,13 +13,14 @@ import { Space } from '../../foundation/styles/variables';
 
 import { ComicViewer } from './internal/ComicViewer';
 
+
 const EpisodeDetailPage: React.FC = () => {
   const { bookId, episodeId } = useParams<RouteParams<'/books/:bookId/episodes/:episodeId'>>();
   invariant(bookId);
   invariant(episodeId);
 
-  const { data: book } = useBook({ params: { bookId } });
   const { data: episode } = useEpisode({ params: { episodeId } });
+  const { data: episodes } = useEpisodeList({ query: { bookId }});
 
   return (
     <Box>
@@ -31,8 +32,8 @@ const EpisodeDetailPage: React.FC = () => {
 
       <Box aria-label="エピソード一覧" as="section" px={Space * 2}>
         <Flex align="center" as="ul" direction="column" justify="center">
-          {book.episodes.map((episode) => (
-            <EpisodeListItem key={episode.id} bookId={bookId} episodeId={episode.id} />
+          {episodes.map((episode) => (
+            <EpisodeListItem key={episode.id} bookId={bookId} episode={episode} />
           ))}
         </Flex>
       </Box>
